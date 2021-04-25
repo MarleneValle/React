c0054e61874c6e8a7418b03c4cd772068354a86d
Marlene Valle <marlenevalle@live.com.mx>
2021-04-25 08:18:13 +0200

    e-commerce project progress

--- e-commerce/src/components/Products/Product/Product.jsx
+++ e-commerce/src/components/Products/Product/Product.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
-import { Card, CardMedia, CardContent, CardAction, Typography, IconButton } from '@material-ui/core';
-import { AddShoppingCard } from '@material-ui/icons'
-import { classes } from 'istanbul-lib-coverage';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons'
 
-const Product = ({ product }) => {
+
+import useStyles from './styles';
+
+
+const Product = ({ product, onAddToCart }) => {
+    const classes = useStyles();
+    
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={ product.media.source } title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
-                    <Typography variant="h5">
-                        {product.price}
+                 </div>
+                <div>
+                    <Typography variant="h6">
+                        {product.price.formatted_with_code}
                     </Typography>
                 </div>
-                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+                <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body2" color="textSecondary" />
             </CardContent>
-            <CardAction disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
-                    <AddShoppingCard />
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart" onClick={() => {onAddToCart(product.id, 1)}}>
+                    <AddShoppingCart />
                 </IconButton>
-            </CardAction>
+            </CardActions>
         </Card>
     );
 }
