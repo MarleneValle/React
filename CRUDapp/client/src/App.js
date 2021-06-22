984a1f1faf5c3809198c7bcf5898e88bae9ae951
Marlene Valle <marlenevalle@live.com.mx>
2021-06-22 14:05:05 +0200

    CRUD app with react, node.js and msql

---
+++ CRUDapp/client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+    <h1>CRUD application</h1>
+      <div className="form">
+        <label htmlFor="">Movie Name</label>
+        <input type="text" name="movieName"/>
+        <label htmlFor="">Review</label>
+        <input type="text" name="review"/>
+
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
