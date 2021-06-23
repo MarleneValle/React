3ab4f2f4a79ef243829ab108bcc9fb828af63d2e
Marlene Valle <marlenevalle@live.com.mx>
2021-06-23 11:37:42 +0200

    funtional app crud...working in the style

--- CRUDapp/server/index.js
+++ CRUDapp/server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const bodyParser = require ('body-parser');
+const cors = require('cors')
 const app = express();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Vahg9030",
+    host: 'localhost',
+    user: 'root',
+    password: 'Vahg9003', 
     database: "CRUDDataBase",
 });
 db.connect((err) => {
@@ -13,14 +15,51 @@ db.connect((err) => {
   console.log('Connected!');
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/", (req, res) =>{
+app.get('/api/get', (req,res) =>{
+  const sqlSelect = 'SELECT * FROM CRUDDataBase.movie_reviews';
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  })
+})
 
-//     const sqlInsert = "INSERT INTO `movie_reviews` (`movieName`, `movieReview`) VALUES ('inception', 'good movie');"  
-//     db.query(sqlInsert, (err, result) =>{
-//         res.send("Hello Marlene from Malmö")
-//     })
-// });
+app.post('/api/insert', (req, res) => {
+
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert = 'INSERT INTO CRUDDataBase.movie_reviews (movieName, movieReview) VALUES (?,?);'
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+  console.log(result);
+  })
+});
+
+app.delete('/api/delete/:movieName', (req,res) =>{
+  const name = req.params.movieName;
+  const sqlDelete = 'DELETE FROM CRUDDataBase.movie_reviews WHERE id = ?';
+
+  db.query(sqlDelete, name, (err, result) =>{
+    if(err)  console.log(err);
+  });
+});
+
+app.put('/api/update', (req,res) => {
+  const id = req.body.id;
+  const review = req.body.movieReview;
+  const sqlUpdate =
+  'UPDATE CRUDDataBase.movie_reviews SET movieReview =? WHERE id = ?';
+
+  db.query(sqlUpdate, [review, id], (err, result) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
 app.listen(3001, () =>{
     console.log("running on port 3001")
